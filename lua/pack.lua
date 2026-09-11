4ae8b6325c698255f038434351ef1316a5299eaf
John Indra <i.john@me.com>
2026-09-11 08:30:21 +0700

    Change keymap
    Inspired by Lazyvim

--- lua/pack.lua
+++ lua/pack.lua
@@ -58,7 +58,7 @@ MiniPick.setup()
 MiniExtra.setup()
 
 -- keymaps
-vim.keymap.set("n", "<leader>pf", function() MiniPick.builtin.files() end, { desc = "Mini File Picker" })
+vim.keymap.set("n", "<leader><space>", function() MiniPick.builtin.files() end, { desc = "Mini File Picker" })
 vim.keymap.set("n", "<leader>ps", function() MiniPick.builtin.grep({ pattern = vim.fn.expand("<cword>") }) end, { desc = "Grep word/Search word" })
 vim.keymap.set("n", "<leader>vh", function() MiniPick.builtin.help() end, { desc = "Mini Help" })
 
